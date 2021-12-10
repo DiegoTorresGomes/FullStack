02ec97b692da170fea2a8022ef1287ef29540454
DiegoTorresGomes <diegotorresgomes@bol.com.br>
2021-12-10 13:30:43 -0300

    CursoPorgramadorBr

---
+++ Javascript Básico/script3.js
@@ -0,0 +1,26 @@
+//+ Adição
+// - Subtração
+// * Miltuplicação
+// / divisão
+// % módulo
+// ++ incremento
+// -- decremento
+
+var a = 10;
+var b = 15;
+
+var c = a + b;
+var d = a - b;
+var e = a * b;
+var f = a / b;
+var g = a % b; //resto da divisão
+var h = ++a; // se colocarmos a++ ele primeiro vai pegar o valor de a e depois vai incrementar
+var i = --b;// se colocarmos b-- ele primeiro vai pegar o valor de b e depois vai decrementar
+
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
