30eeda20feb538956dee931604fb013ad5f902d1
DiegoTorresGomes <diegotorresgomes@bol.com.br>
2021-12-12 13:28:03 -0300

    CursoPorgramadorBr

---
+++ Javascript Básico/script5.js
@@ -0,0 +1,20 @@
+// == igual a
+// === valor igual e igual
+// != não é igual
+// !== não igual ou não igual
+// > maior que
+// < menor que
+// >= maior que ou igual a
+// <= menor que ou igual a
+
+var a = 2;
+var b = "2";
+
+console.log(a == b);
+console.log(b === a);
+console.log(a != b);
+console.log(a !== b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
