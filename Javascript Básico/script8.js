f2c56622c9baa1b41aa0a3c7bd3c4557b7a2c1f2
DiegoTorresGomes <diegotorresgomes@bol.com.br>
2022-06-29 19:07:26 -0300

    aula9

---
+++ Javascript Básico/script8.js
@@ -0,0 +1,61 @@
+// operadores lógicos
+
+//&& e
+// || ou
+// negativo
+
+/*var a = true;
+var b = true;
+
+var c = a && b;
+
+console.log(c);
+*/
+
+
+// && e
+/*var idade = 35;
+
+var maior20 = idade > 20;
+var menor30 = idade < 30;
+
+var entre = maior20 && menor30
+
+console.log("Idade ", idade);
+
+console.log("Maior do que vinte ", maior20);
+
+console.log("Meor que trinta ", menor30);
+
+console.log("Entre 20 e 30 ", entre);
+*/
+
+
+// || ou 
+/*var idade = 9;
+
+var maior65 = idade >= 65;
+var menor10 = idade <= 10;
+
+var gratuidade = maior65 || menor10
+
+console.log("Idade ", idade);
+
+console.log("Maior do que 65 ", maior65);
+
+console.log("Meor que 10 ", menor10);
+
+console.log("Tem direito a gratuidade ? ", gratuidade);
+*/
+
+// negativo
+var idade = 35;
+
+var maior20 = idade > 20;
+var menor30 = !maior20;
+
+
+console.log("Maior do que vinte ", maior20);
+
+console.log("Menor que trinta ", menor30);
+
