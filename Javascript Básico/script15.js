20dde8f37d588ea7f59115d3cdea063dc3d00380
DiegoTorresGomes <diegotorresgomes@bol.com.br>
2022-07-07 17:55:50 -0300

    Curso Programador BR

---
+++ Javascript Básico/script15.js
@@ -0,0 +1,37 @@
+// condicionais switch
+
+/*var numero = 5;
+
+for(var vez = 0; vez < numero; vez++ ){
+
+    console.log("Executando o for, pela " + (vez + 10) + " Vez")
+
+
+}
+
+console.log("Acabou")
+*/
+
+
+/*var numero = 5;
+
+while(numero < 10){
+
+    console.log("Número " + numero);
+    numero++
+
+}
+
+console.log("Acabou")
+*/
+
+var numero = Math.round() * 100; //cria um número randomico
+
+while(numero < 90){
+
+    console.log(numero);
+    numero = Math.random() * 100;
+
+}
+
+console.log("Acabou")
