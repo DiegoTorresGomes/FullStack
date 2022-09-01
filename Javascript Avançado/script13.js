45b10eb36946cf0203b2f071db94cc62c8f9a924
DiegoTorresGomes <diegotorresgomes@bol.com.br>
2022-09-01 17:59:21 -0300

    JS Avançado - API

---
+++ Javascript Avançado/script13.js
@@ -0,0 +1,38 @@
+//fetch
+
+let url = "https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL"
+
+
+
+
+
+
+
+
+
+
+ function converter() {
+    let input = document.getElementById("valor");
+    let valor = input.value;
+
+    fetch(url)
+    .then((ress) =>{
+        return ress.json();
+
+    })
+
+    .then((data)=> {
+        
+        let rate = data.USDBRL.high;
+
+        let resultado = `${valor} dolares = ${rate * valor} em reais`
+
+        document.getElementById("resultado").innerHTML = resultado;
+
+    })
+
+}
+
+
+   
+
