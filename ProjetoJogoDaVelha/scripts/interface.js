47d4e8e25318cfd8f46801f9ab89135c4532ecce
DiegoTorresGomes <diegotorresgomes@bol.com.br>
2022-09-05 21:26:58 -0300

    ProjetoJogoDaVelha

---
+++ ProjetoJogoDaVelha/scripts/interface.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square)=>{
+        square.addEventListener('click', handleClick);
+    })
+
+
+
+
+})
+
+function handleClick(event){
+
+    console.log(event.target);
+
+    let square = event.target;
+    let postion = square.id;
+
+    handleMove(postion);
+    updateSquares();
+
+}
+
+function updateSquares() {
+
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+        let postion = square.id;
+        let symbol = board[postion];
+
+        if(symbol != '') {
+            square.innerHTML = `<div class='${symbol}'></div>`
+        }
+    })
+}
