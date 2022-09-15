14a345ea4fbd1859bb8f0070ed7ce678db464657
DiegoTorresGomes <diegotorresgomes@bol.com.br>
2022-09-15 18:35:01 -0300

    JogoDaMemoria

--- ProjetoJogoDaMemoria/script.js
+++ ProjetoJogoDaMemoria/script.js
@@ -1,5 +1,7 @@
 const FRONT = "card_front";
 const BACK = "card_back";
+const CARD = "card";
+const ICON = "icon";
 
 let techs = [
     'bootstrap',
@@ -20,6 +22,46 @@ startGame();
 function startGame() {
     cards = createCardsFromTechs(techs);
     shuffleCards(cards);
+
+    initializeCards(cards);
+}
+
+function initializeCards(cards){
+    let gameBoard = document.getElementById("gameBoard");
+
+    cards.forEach(card=>{
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement);
+        
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+    })
+
+}
+
+function createCardContent(card, cardElement){
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+}
+
+function createCardFace(face, card, element){
+
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+    if(face === FRONT){
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = "./Assets/images/" + card.icon + ".png";
+        cardElementFace.appendChild(iconElement);
+    } else {
+        cardElementFace.innerHTML = "&lt/&gt";
+    }
+
+    element.appendChild(cardElementFace);
 }
 
 function shuffleCards(cards){
@@ -64,4 +106,8 @@ function createPairFromTech(tech){
 
 function createIdWithTech(tech){
     return tech + parseInt(Math.random() * 1000);
+}
+
+function flipCard () {
+    this.classList.add("flip");
 }
